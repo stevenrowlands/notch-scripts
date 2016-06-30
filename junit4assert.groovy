08b8a5d1cb5412c905ecad8996178da427ec9595
Steven Rowlands <stevenrowlands@gmail.com>
2016-06-30 19:17:42 +1000

    scripts working

--- junit4assert.groovy
+++ junit4assert.groovy
@@ -1,15 +1,17 @@
 
 def line = text.getPrecedingLineText();
-
+console.write("Hello World");
 String [] macros = line.split(" ");
-if (macros[1] == "=") {
-    text.removePrecedingLineText();
-    java.addStaticImport("org.junit.Assert.assertThat");
-    java.addStaticImport("org.hamcrest.CoreMatchers.equalTo");
-    java.template("assertThat(" + macros[0] + ",equalTo(" + macros[2] + "));");
-} else if (macros[1] == ">") {
-    text.removePrecedingLineText();
-    java.addStaticImport("org.junit.Assert.assertThat");
-    java.addStaticImport("org.hamcrest.Matchers.greaterThan");
-    java.template("assertThat(" + macros[0] + ",greaterThan(" + macros[2] + "));\${cursor}");
+
+if (macros.length == 3) {
+	String operator;
+	java.addStaticImport("org.junit.Assert.assertThat");
+	text.removePrecedingLineText();
+	if (macros[1] == "=") {
+	    java.addStaticImport("org.hamcrest.CoreMatchers.equalTo");
+	    java.template("assertThat(" + macros[0] + ",equalTo(" + macros[2] + "));");
+	} else if (macros[1] == ">") {
+	    java.addStaticImport("org.hamcrest.Matchers.greaterThan");
+	    java.template("assertThat(" + macros[0] + ",greaterThan(" + macros[2] + "));\${cursor}");
+	}
 }
--- junit4switch.groovy
+++ junit4switch.groovy
@@ -1,15 +1,20 @@
 String packageName = java.packageName;
 String content =  "package " + packageName + ";"
 String className = java.name + "Test";
-if (java.name.contains("Test")) {
+console.write(java.relativeName);
+if (java.relativeName.contains("src/test/java")) {
+    javaproject.addFile(java.relativeName.replace("src/test/java","src/main/java").replace("Test.java", ".java"),content);
+    return;
+} else if (java.relativeName.contains("Test.java")) {
     javaproject.addFile(java.relativeName.replace("test","src").replace("Test.java", ".java"),content);
     return;
 }
 
 content = content + "\n" + "public class " + className + "{\n}";
-javaproject.addFile(java.relativeName.replace("src","test").replace(".java", "Test.java"),content);
+if (java.relativeName.contains("src/main/java")) {
+ javaproject.addFile(java.relativeName.replace("src/main/java","src/test/java").replace(".java", "Test.java"),content);
+} else {
+ javaproject.addFile(java.relativeName.replace("src","test").replace(".java", "Test.java"),content);
+}
 
 java.addImport("org.junit.Test");
-if (java.getMethod("test") == null) {
-	java.addMethod("@Test\npublic void test() {}");
-}
